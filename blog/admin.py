d7787a11ef3976539446c6eb01f772dff4f5ac27
PolkanDo <mrmarmon@mail.ru>
2022-08-22 18:43:49 +0300

    second commit

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Post)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'status', 'slug', 'author')
+    prepopulated_fields = {'slug': ('title',), }
